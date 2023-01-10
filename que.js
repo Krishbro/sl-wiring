d2940d628429ca89dad4615cf3b44528d1a90eef
Krishbro <82700919+Krishbro@users.noreply.github.com>
2023-01-10 22:42:11 +0530

    Add files via upload

---
+++ que.js
@@ -0,0 +1,114 @@
+var q1="ධාරාවක් යනු ඉලෙක්ට්‍රෝන වල ගල යෑමකි."
+var questions =[
+    {
+        num : 1,
+        question : q1,
+        answer : "ඉලෙක්ට්‍රෝන වල ගලා යෑමකි.",
+        options : [
+            "ඉලෙක්ට්‍රෝන වල ගලා යෑමකි.",
+            "B) to wear",
+            "C) wearing",
+            "D) wears"
+        ]
+    },
+    {
+num: 2,
+question: "My father _____ his car every weekend.",
+answer: "B) washes",
+options: [
+"A) wash",
+"B) washes",
+"C) washing",
+"D) washed"
+]
+},
+{
+num: 3,
+question: "My Mom _____ at 6 o&sbquo;clock everyday.",
+answer: "C) gets up",
+options: [
+"A) stands up",
+"B) looks up",
+"C) gets up",
+"D) jumps up"
+]
+},
+
+{
+num: 4,
+question: "Lakmal is a postman. He _____ letters to people.",
+answer: "C) delivers",
+options: [
+"A) answers",
+"B) works",
+"C) delivers",
+"D) serves"
+]
+},
+{
+num: 5,
+question: "Kamal is a doctor. He _____ ill people.",
+answer: "C) helps",
+options: [
+"A) looks at",
+"B) speaks to",
+"C) helps",
+"D) serves drinks"
+]
+},
+{
+num: 6,
+question: "Most of the people _____ work at 8 o’clock every morning.",
+answer: "D) starts",
+options: [
+"A) finishes",
+"B) goes",
+"C) does",
+"D) starts"
+]
+},
+{
+num: 7,
+question: "_____ does your brother do in his free time?",
+answer: "D) What",
+options: [
+"A) Why",
+"B) What kind",
+"C) How many",
+"D) What"
+]
+},
+{
+num: 8,
+question: "She _____ from England.",
+answer: "B) comes",
+options: [
+"A) come",
+"B) comes",
+"C) don’t come",
+"D) goes"
+]
+},
+{
+num: 9,
+question: "Our teacher gives _____ a lot of homework.",
+answer: "B) us",
+options: [
+"A) our",
+"B) us",
+"C) her",
+"D) his"
+]
+},
+{
+num: 10,
+question: "He drives the children _____ school.",
+answer: "C) to",
+options: [
+"A) of",
+"B) at",
+"C) to",
+"D) go"
+]
+},
+];
